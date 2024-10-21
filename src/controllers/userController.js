ddd05a648f10f5d6d20861c8cbc67299ed65bb72
El-labo <jcwrubu@gmail.com>
2024-10-21 11:39:35 +0900

    2nd commit

---
+++ src/controllers/userController.js
@@ -0,0 +1,6 @@
+export const join = (req, res) => res.send("join");
+export const login = (req, res) => res.send("Log In");
+export const edit = (req, res) => res.send("Edit user");
+export const remove = (req, res) => res.send("Delete User");
+export const logout = (req, res) => res.send("Log Out");
+export const see = (req, res) => res.send("User ID");
